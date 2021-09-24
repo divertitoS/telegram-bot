615a7413c924ea5862f724b9e990e2067a84a305
divertitoS <astanislav2361@gmail.com>
2021-09-24 21:38:34 +0300

    first commit

---
+++ src/main/java/com/dev/bot/telegram/TelegramBotApplication.java
@@ -0,0 +1,12 @@
+package com.dev.bot.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dev/bot/telegram/TelegramBotApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dev.bot.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
